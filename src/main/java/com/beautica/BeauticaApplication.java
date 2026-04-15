eb2ea6e858dace7b3261c314e35d7ffbeac34d81
Denys Zvarych <zvarych.d.v@gmail.com>
2026-04-15 14:02:35 +0000

    update proejct structure

---
+++ src/main/java/com/beautica/BeauticaApplication.java
@@ -0,0 +1,12 @@
+package com.beautica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeauticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeauticaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/beautica/BeauticaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.beautica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("local")
+class BeauticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
